6edb7f847908a08271edd65935e9bec9163527c3
Andrew Yip <yipcma@gmail.com>
2016-08-22 18:14:04 +0300

    learnyounode #8 core bl http.get response.pipe error-handling

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,12 +1,14 @@
 var http = require('http');
+var bl = require('bl');
 
 var url = process.argv[2];
 
 http.get(url, function callback (response) {
-  // setEncoding to avoid toString()
-  response.setEncoding('utf8');
-  // listen to event; callback function simplified
-  response.on('data', console.log);
-  // error handling
-  response.on('error', console.error);
-}).on('error', console.error);
+  response.pipe(bl(function (err, data) {
+    if (err)
+      return console.error(err);
+    data = data.toString();
+    console.log(data.length);
+    console.log(data);
+  }))
+})
