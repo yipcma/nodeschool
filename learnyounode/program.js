6534b47c62070cb2a5d9c69506280ff10e4316cf
Andrew Yip <yipcma@gmail.com>
2016-08-22 19:37:19 +0300

    learnyounode #9 core bl async-juggle http.get response.pipe

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,14 +1,31 @@
 var http = require('http');
 var bl = require('bl');
+var results = [];
+var done = 0;
+var urls = process.argv.slice(2, process.argv.length);
 
-var url = process.argv[2];
+function printAll () {
+  for (var i = 0; i < urls.length; i++) {
+    console.log(results[i]);
+  }
+}
 
-http.get(url, function callback (response) {
-  response.pipe(bl(function (err, data) {
-    if (err)
-      return console.error(err);
-    data = data.toString();
-    console.log(data.length);
-    console.log(data);
-  }))
-})
+function getData (i) {
+    http.get(urls[i], function callback (response) {
+      response.pipe(bl(function (err, data) {
+        if (err)
+          return console.error(err);
+        data = data.toString();
+        results[i] = data;
+        // the counter lives inside the callback
+        done++;
+        if (done === urls.length) {
+          printAll();
+        }
+      }))
+    }).on('error', console.error);
+}
+
+for (var i = 0; i < urls.length; i++) {
+  getData(i);
+}
