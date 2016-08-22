09e4116ecf314228a7acccdfb73b7cdaac9c03dc
Andrew Yip <yipcma@gmail.com>
2016-08-23 02:51:29 +0300

    learnyounode #12 http.createServer through2-map req.pipe(map(cb)).pipe(res) req.method res.end server.listen(port)

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,12 +1,15 @@
 var http = require('http');
-var fs = require('fs');
+var map = require('through2-map');
 
 var port = process.argv[2];
-var file = process.argv[3];
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'content-type': 'text/plain'});
-  fs.createReadStream(file).pipe(res);
+  if (req.method !== 'POST')
+    return res.end('send me a POST\n');
+
+  req.pipe(map(function(chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(res);
 })
 
 server.listen(port);
