ee78cb71905704201188607ae09f31abb19bd588
Andrew Yip <yipcma@gmail.com>
2016-08-23 02:30:04 +0300

    learnyounode #11 http.createServer res.writeHead fs.createReadStream(file).pipe(res) server.listen(port)

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,11 +1,12 @@
-var net = require('net');
-var strftime = require('strftime');
-var port = process.argv[2];
+var http = require('http');
+var fs = require('fs');
 
-var server = net.createServer(function (socket) {
-  // new Date() implied in second argument to strftime
-  var val = strftime('%F %H:%M') + '\n';
-  socket.end(val);
+var port = process.argv[2];
+var file = process.argv[3];
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'content-type': 'text/plain'});
+  fs.createReadStream(file).pipe(res);
 })
 
 server.listen(port);
