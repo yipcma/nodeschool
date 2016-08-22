b24bde315c09e9dd746378db3d6d3ae11a97dba4
Andrew Yip <yipcma@gmail.com>
2016-08-22 13:53:27 +0300

    learnyounode #4 fs readFile (ASync) callback 'utf8' toString split

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,7 +1,11 @@
-var fs = require('fs')
+var fs = require('fs');
+// put second argument as 'utf8' to get a String instead of a Buffer
 
-var buf = fs.readFileSync(process.argv[2])
-
-var str = buf.toString().split('\n')
-
-console.log(str.length - 1)
+fs.readFile(process.argv[2], 'utf8', function readFileCallback (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    var val = data.split('\n').length - 1;
+    console.log(val);
+  }
+})
