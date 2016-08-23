e64f677bd74e030eb3f26ed1a0201a142f39b1df
Andrew Yip <yipcma@gmail.com>
2016-08-23 13:16:38 +0300

    learnyounode #13 try#1 http.createServer res.end url.parse req.method res.writeHead Date.getTime

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,15 +1,27 @@
 var http = require('http');
-var map = require('through2-map');
+var url = require('url');
 
 var port = process.argv[2];
 
 var server = http.createServer(function(req, res) {
-  if (req.method !== 'POST')
-    return res.end('send me a POST\n');
-
-  req.pipe(map(function(chunk) {
-    return chunk.toString().toUpperCase();
-  })).pipe(res);
+  if (req.method !== 'GET')
+    return res.end('not a GET\n');
+  var route = url.parse(req.url, true);
+  var time = new Date(route.query.iso);
+  if (route.pathname === '/api/parsetime') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({
+      hour: time.getHours(),
+      minute: time.getMinutes(),
+      second: time.getSeconds(),
+    }));
+  }
+  if (route.pathname === '/api/unixtime') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({
+      unixtime: time.getTime(),
+    }));
+  }
 })
 
 server.listen(port);
