2ce0f8d304dacabea49f566d1dd86e10a5749c21
Andrew Yip <yipcma@gmail.com>
2016-08-23 13:22:35 +0300

    learnyounode #13 try#2 modularize results and handle 404

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -3,24 +3,40 @@ var url = require('url');
 
 var port = process.argv[2];
 
+var result;
+
+function parseTime(time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds(),
+  }
+}
+
+function unixTime(time) {
+  return {
+    unixtime: time.getTime(),
+  }
+}
+
 var server = http.createServer(function(req, res) {
   if (req.method !== 'GET')
     return res.end('not a GET\n');
   var route = url.parse(req.url, true);
   var time = new Date(route.query.iso);
   if (route.pathname === '/api/parsetime') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-      hour: time.getHours(),
-      minute: time.getMinutes(),
-      second: time.getSeconds(),
-    }));
+    result = parseTime(time);
   }
   if (route.pathname === '/api/unixtime') {
+    result = unixTime(time);
+  }
+  if (result) {
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-      unixtime: time.getTime(),
-    }));
+    // res.write(data); res.end(); => res.end(data);
+    res.end(JSON.stringify(result));
+  } else {
+    res.writeHead(404);
+    res.end();
   }
 })
 
